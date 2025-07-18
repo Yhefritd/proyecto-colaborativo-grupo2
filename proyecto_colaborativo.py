d771a8e4e05de9deadf2003c985930fd9d4f2179
Jair <2jairurbano@gmail.com>
2025-07-18 12:30:25 -0500

    Agregar archivo proyecto_colaborativo.py

---
+++ proyecto_colaborativo.py
@@ -0,0 +1,16 @@
+# Archivo: proyecto_colaborativo.py
+
+def presentar_grupo():
+    integrantes = [
+        "Yhefritd",
+        "Fernando",
+        "Yerchelo",
+        "Jair"
+    ]
+    print("Proyecto colaborativo del grupo 2")
+    print("Integrantes:")
+    for nombre in integrantes:
+        print(f"- {nombre}")
+
+if __name__ == "__main__":
+    presentar_grupo()
